b0d9032ecb8edf7246e398552bda9e58f185e683
Maciej <m.szymeczko@dac.digital>
2023-03-27 00:58:15 +0200

    added edit request endpoint

--- src/main/kotlin/com/me/postfetcher/route/MainRouting.kt
+++ src/main/kotlin/com/me/postfetcher/route/MainRouting.kt
@@ -13,8 +13,10 @@ import com.me.postfetcher.route.dto.HabitDto
 import com.me.postfetcher.route.dto.HabitEditRequest
 import com.me.postfetcher.route.dto.HabitsResponse
 import com.me.postfetcher.service.PostsFetcher
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
+import io.ktor.server.plugins.origin
 import io.ktor.server.request.receive
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
@@ -27,7 +29,9 @@ private val logger = mu.KotlinLogging.logger {}
 fun Route.mainRouting(
     postsFetcher: PostsFetcher
 ) {
+
     get("/habits") {
+        call.response.headers.append(HttpHeaders.AccessControlAllowOrigin, "*")
         val response =
             either<AppError, HabitsResponse> {
                HabitsResponse(fetchHabits().map { it.toDto() })
