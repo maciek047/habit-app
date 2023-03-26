4a517c326ccf447b4564cb88786143ac557b9ad2
Maciej <m.szymeczko@dac.digital>
2023-03-26 21:37:01 +0200

    fix fetchHabits

--- src/main/kotlin/com/me/postfetcher/route/MainRouting.kt
+++ src/main/kotlin/com/me/postfetcher/route/MainRouting.kt
@@ -14,15 +14,31 @@ import io.ktor.server.application.call
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 
+
+private val logger = mu.KotlinLogging.logger {}
+
+
 fun Route.mainRouting(
     postsFetcher: PostsFetcher
 ) {
     get("/habits") {
         val response =
             either<AppError, HabitsResponse> {
+                logger.info("Fetching habits")
+                val habits = fetchHabits()
+                logger.info("Fetched habits: $habits")
                 HabitsResponse(fetchHabits().map { it.toDto() })
             }.toApiResponse(HttpStatusCode.OK)
         call.apiResponse(response)
     }
+
+    get("/test") {
+        val response =
+            either<AppError, PostsResponse> {
+               PostsResponse("It works!")
+            }.toApiResponse(HttpStatusCode.OK)
+        call.apiResponse(response)
+    }
+
 
 }
