f4a4fb9f2ca0e5ba57cd91a3250a5b38db5839c7
Maciej <m.szymeczko@dac.digital>
2023-04-27 00:07:55 +0200

    added user authentication

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
     application
     kotlin("jvm") version "1.7.0"
 //    kotlin("plugin.serialization") version "1.7.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("com.heroku.sdk.heroku-gradle") version "3.0.0"
 }
