c27e9aa68bf0e7dbfc73ee07f0cf7d87a1e2742e
Maciej <m.szymeczko@dac.digital>
2023-05-02 17:44:25 +0200

    added user authentication

--- src/main/kotlin/com/me/postfetcher/Application.kt
+++ src/main/kotlin/com/me/postfetcher/Application.kt
@@ -86,13 +86,14 @@ fun Application.setup(dep: Dependencies) {
         }
     }
 
-    val jwkProvider = JwkProviderBuilder(System.getenv("AUTH0_DOMAIN"))
+    val jwkProvider = JwkProviderBuilder("https://$domain/")
         .cached(10, 24, TimeUnit.HOURS)
         .rateLimited(10, 1, TimeUnit.MINUTES)
         .build()
 
     fun validateCreds(credential: JWTCredential): JWTPrincipal? {
         val containsAudience = credential.payload.audience.contains(System.getenv("AUTH0_AUDIENCE"))
+        println("credential.payload.audience: ${credential.payload.audience.joinToString()}")
 
         if (containsAudience) {
             return JWTPrincipal(credential.payload)
